ae417eec86c121788ff3f408deded898619e7eac
seanhodges61 <seanhodges01@gmail.com>
2018-04-05 15:53:46 -0400

    Main server fixes

--- SecureFolder-Host/Connection.c
+++ SecureFolder-Host/Connection.c
@@ -4,8 +4,9 @@
 #include <errno.h>
 #include <FifoRead.h>
 #include <FifoWrite.h>
+#include <secureFolder.h>
 
-int do_read (char* details) {
+int do_read (char* details, int* isChange) {
   char call_type [MAX_CALL_LEN];
   char file_path [MAX_FILE_PATH];
   char file_id   [UUID_LEN];
@@ -16,14 +17,19 @@ int do_read (char* details) {
   get_file_id (file_path, file_id);
 
   if (strcmp (call_type, CMD_ADD_USER) == 0) {
+    *isChange = 1;
     return f_add_user (file_id, details);
   } else if (strcmp (call_type, CMD_REVOKE_ACCESS) == 0) {
+    *isChange = 1;
     return f_revoke_access (file_id, details);
   } else if (strcmp (call_type, CMD_REMOVE_FILE) == 0) {
+    *isChange = 1;
     return f_remove_file_security (file_id, details);
   } else if (strcmp (call_type, CMD_CAN_ACCESS) == 0) {
+    *isChange = 0;
     return f_user_can_access (file_id, details);
   } else {
+    *isChange = 0;
     printf ("Could not find command type: %s\n", call_type);
     return 0;
   }
@@ -40,6 +46,10 @@ int main () {
   char * myfifo = SERVER_FIFO;
   char buf[4096];
 
+  int isChange;
+
+  secure_folder_load ();
+
   /* open, read, and display the message from the FIFO */
   while (1) {
     int ret = fifo_read (myfifo, buf, 4096);
@@ -49,12 +59,13 @@ int main () {
       continue;
     }
 
-    int resp = do_read (buf);
-
-    printf ("Command is %s\n", buf);
-    printf ("Return value is %d\n", resp);
+    int resp = do_read (buf, &isChange);
     respond (resp);
+
+    if (isChange)
+      secure_folder_save();
   }
+
 
   return 0;
 }
--- SecureFolder-Host/functions.c
+++ SecureFolder-Host/functions.c
@@ -8,19 +8,23 @@ int f_add_user (char* fileId, char* s) {
   int canRead  = read_int (&s);
   int canWrite = read_int (&s);
   int canExec  = read_int (&s);
-  
+
   // in case not already in the secure folder
   add_to_secure_folder (fileId);
 
   // add the user Permissions
   int status = add_user_access (fileId, uid, canRead, canWrite, canExec);
+
   return status == 0;
 }
 
 int f_revoke_access (char* fileId, char* s) {
   int uid = read_int (&s);
-  int status = revoke_access (fileId, uid);
 
+  int canAccess = user_can_access (fileId, uid);
+  if (!canAccess) return 0;
+
+  int status = revoke_access (fileId, uid);
   return status == 0;
 }
 
